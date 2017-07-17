d2da54a13f09cd3a2b07bda7d9ca331378d12db6
Jérôme SAMSON DIT LAVIGNE <jerome.samson07@gmail.com>
2017-07-17 13:17:34 +0200

    Initialisation dépôt et ajout du code log

---
+++ src/main/java/dev/log_SLF4J/App.java
@@ -0,0 +1,18 @@
+package dev.log_SLF4J;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+	public static void main(String[] args) {
+		LOG.info("Bonjour Slf4J !");
+		LOG.info("Implémentation Logback");
+		new AppService().executer("valeur 1");
+	}
+}
